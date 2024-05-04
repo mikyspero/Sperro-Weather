bab841d05ca4872c4245e2d251474f1e08005fc8
mikyspero <mikyspero@gmail.com>
2024-05-04 16:44:27 +0200

    bigfix: corrected error handler middleware

--- backend/src/middlewares/error-handling.ts
+++ backend/src/middlewares/error-handling.ts
@@ -1,20 +1,24 @@
-import { Request, Response } from "express";
+import { Request, Response, NextFunction } from "express";
 import { WebError } from "../utils/webError";
 
-const errorHandler = (err: any, req: Request, res: Response) => {
+const errorHandler = (
+  err: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   if (err instanceof WebError) {
     // Handle known WebError types
     const statusCode = err.status || 500; // Default to 500 Internal Server Error if statusCode is not set
     const message = err.message || "Internal Server Error";
-
     return res.status(statusCode).json({ error: message });
   }
   // Handle other types of errors (unexpected errors)
   console.error("Unhandled error:", err);
   // Set a generic error response
-  const statusCode = err.statusCode || 500;
+  const statusCode = 500;
   const message = "Internal Server Error";
-  return res.status(statusCode).json({ error: message });
+  res.status(statusCode).json({ error: message });
 };
 
 export { errorHandler };
