da7839d6831f3e1995426087e8e2a62ba54b3671
mikyspero <mikyspero@gmail.com>
2024-05-10 01:45:59 +0200

    Refactor: cleaned up the routers a little, re habilitated validation middleware

--- backend/dist/routes/coordinate-routes.js
+++ backend/dist/routes/coordinate-routes.js
@@ -3,15 +3,10 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.weatherRouter = void 0;
 const express_1 = require("express");
 const weatherController_1 = require("../controllers/weatherController");
+const typechecking_1 = require("../middlewares/typechecking");
 const weatherRouter = (0, express_1.Router)();
 exports.weatherRouter = weatherRouter;
 //mount middleware for coordinates validation
-//weatherRouter.use(checkCoordinates);
-// Route for current weather
-//weatherRouter.get("/current", getCurrentWeatherHandler);
-// Route for hourly weather
-//weatherRouter.get("/hourly", getHourlyWeatherHandler);
-// Route for daily weather
-//weatherRouter.get("/daily", getDailyWeatherHandler);
+weatherRouter.use(typechecking_1.checkCoordinates);
 // a single route now handles every weather case using parameter parsing
 weatherRouter.get("/:weather", weatherController_1.getWeatherByParam);
--- backend/src/routes/coordinate-routes.ts
+++ backend/src/routes/coordinate-routes.ts
@@ -1,21 +1,13 @@
 import { Router } from "express";
 import {
-
   getWeatherByParam,
 } from "../controllers/weatherController";
 import { checkCoordinates } from "../middlewares/typechecking";
 
 const weatherRouter = Router();
 //mount middleware for coordinates validation
-//weatherRouter.use(checkCoordinates);
-// Route for current weather
-//weatherRouter.get("/current", getCurrentWeatherHandler);
+weatherRouter.use(checkCoordinates);
 
-// Route for hourly weather
-//weatherRouter.get("/hourly", getHourlyWeatherHandler);
-
-// Route for daily weather
-//weatherRouter.get("/daily", getDailyWeatherHandler);
 // a single route now handles every weather case using parameter parsing
 weatherRouter.get("/:weather", getWeatherByParam);
 export { weatherRouter };
