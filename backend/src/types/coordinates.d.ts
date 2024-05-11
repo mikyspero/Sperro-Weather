d5089f33af7125301de17ce56fe52083a3617cfd
mikyspero <mikyspero@gmail.com>
2024-05-11 23:15:05 +0200

    Refactor: Type Coordinates is now inferred from its schema

--- backend/src/types/coordinates.d.ts
+++ backend/src/types/coordinates.d.ts
@@ -1,4 +1,3 @@
-export type Coordinates = {
-    latitude: number;
-    longitude: number;
-}
+import { z } from "zod";
+import { coordinatesSchema } from "../models/coordinates-schema";
+export type Coordinates = z.infer<typeof coordinatesSchema>;
